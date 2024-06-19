a429eaa7856f183bc1127a76fd39fd6b28f9968d
Sondos2001 <sondoal@ac.sce.ac.il>
2024-06-19 20:25:55 +0300

    editing reports list

--- lib/screens/reportlist.dart
+++ lib/screens/reportlist.dart
@@ -1,4 +1,5 @@
- import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/screens/utilities.dart';
 import 'welcome_screen.dart';
@@ -10,7 +11,8 @@ class ReportListScreen extends StatefulWidget {
 
 class _ReportListScreenState extends State<ReportListScreen> {
   List<String> collections = ['Sanitation', 'environment','garden',
-                              'road','lighting','safety','Utilities','transportation']; // Add your collection names here
+                              'road','lighting','safety','Utilities','transportation'
+  ]; // Add your collection names here
 
   String selectedCollection = 'Sanitation'; // Initial collection
 
@@ -19,7 +21,10 @@ class _ReportListScreenState extends State<ReportListScreen> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text('Report List'),
+        title: Text(
+          'Report List',
+          style: TextStyle(fontSize: 24),
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -33,7 +38,11 @@ class _ReportListScreenState extends State<ReportListScreen> {
         ],
       ),
       body: Container(
-        color: Colors.blueGrey[100], // Set the background color here
+        color: const Color.fromARGB(255, 135, 164, 176),
+        child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView( 
+        scrollDirection: Axis.horizontal,
         child: Column(
           children: [
             DropdownButton(
@@ -47,57 +56,147 @@ class _ReportListScreenState extends State<ReportListScreen> {
               items: collections.map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
-                  child: Text(value),
+                  child: Text(
+                    value,
+                    style: TextStyle(fontSize: 18),
+                    ),
                 );
               }).toList(),
             ),
             StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection(selectedCollection).snapshots(),
+              stream: FirebaseFirestore.instance
+              .collection(selectedCollection)
+              .where('sender', isNotEqualTo: '')
+              .snapshots(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
                 }
 
                 if (!snapshot.hasData || snapshot.data!.docs.isEmpty ?? true) {
-                  return Center(child: Text('No reports available.'));
+                  return Center(child: Text(
+                    'No reports available.',
+                    style: TextStyle(fontSize: 18),));
                 }
 
                 final reports = snapshot.data!.docs.reversed.toList();
 
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: reports.length,
-                    itemBuilder: (context, index) {
-                      var report = reports[index];
-                      return Column(
-                        children: [
-                          ListTile(
-                            title: Text(report['sender']),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(report['address']),
-                                Text(selectedCollection, style: TextStyle(fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                            trailing: Text(report['Report']),
-                          ),
-                          Divider(),
-                        ],
-                      );
-                    },
-                  ),
-                );
-              },
+                             return SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      child: ListView.builder(
+                        itemCount: reports.length,
+                        itemBuilder: (context, index) {
+                          var report = reports[index];
+                          return ReportItem(
+                            report: report,
+                            selectedCollection: selectedCollection,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
-    ); 
+    );
   }
   Future<bool> doesCollectionExist(String collectionName) async {
     var collectionRef = FirebaseFirestore.instance.collection(collectionName);
     var docSnapshot = await collectionRef.limit(1).get();
     return docSnapshot.docs.isNotEmpty;
+  }
+}
+
+class ReportItem extends StatefulWidget {
+  final QueryDocumentSnapshot report;
+  final String selectedCollection;
+
+  ReportItem({required this.report, required this.selectedCollection});
+
+  @override
+  _ReportItemState createState() => _ReportItemState();
+}
+
+class _ReportItemState extends State<ReportItem> {
+  String? _selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Text(
+              widget.report['sender'],
+              style: TextStyle(fontSize: 16), // Increase text size here
+            ),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Text(
+                  widget.report['address'],
+                  style: TextStyle(fontSize: 16), // Increase text size here
+                ),
+              ),
+              Text(
+                widget.selectedCollection,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14), // Increase text size here
+              ),
+            ],
+          ),
+          trailing: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Text(
+              widget.report['Report'],
+              style: TextStyle(fontSize: 14), // Increase text size here
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Radio<String>(
+              value: 'Option 1',
+              groupValue: _selectedValue,
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedValue = value;
+                });
+              },
+            ),
+            Text('Not yet treated'),
+            Radio<String>(
+              value: 'Option 2',
+              groupValue: _selectedValue,
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedValue = value;
+                });
+              },
+            ),
+            Text('In treatment'),
+            Radio<String>(
+              value: 'Option 3',
+              groupValue: _selectedValue,
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedValue = value;
+                });
+              },
+            ),
+            Text('Done'),
+          ],
+        ),
+        Divider(),
+      ],
+    );
   }
 }
