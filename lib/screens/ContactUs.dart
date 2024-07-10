1d49d3365c7f102cfd127afe9c89e8900a792457
77abood <mhmd.abood.2001@gmail.com>
2024-07-10 11:06:25 +0300

    add anumber phone

--- lib/screens/ContactUs.dart
+++ lib/screens/ContactUs.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: unused_import, file_names, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:final_project/screens/category_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsScreen extends StatelessWidget {
+  final String phoneNumber = '076-777-3532';
+  final String email = 'contactus@municipality.com';
+  final String emergencyPhoneNumber = '076-777-3532';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,36 +30,54 @@ class ContactUsScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Email: contactus@municipality.com',
+                    'Email: $email',
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
                   Text(
-                    'Phone: +1-076-777-7777',
+                    'Phone: +1-$phoneNumber',
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
-                  SizedBox(height: 20),
                   Text(
                     'Operating Hours:',
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                   Text(
                     'Sunday to Thursday: 9:00 AM to 5:00 PM',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(height: 20),
-                  SizedBox(height: 20),
+                  SizedBox(height: 5),
                   Text(
                     'Response Time: We aim to respond to fault reports within 5 business days.',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 25),
                   Text(
-                    'Emergency Information: In case of emergencies, please call: 076-777-3532.',
+                    'Emergency Information:',
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 10),
+                  Text(
+                    'In case of emergencies, please call:',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final phoneUri = Uri.parse('tel:$emergencyPhoneNumber');
+                      if (await canLaunchUrl(phoneUri)) {
+                        await launchUrl(phoneUri);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(
+                                  'Could not launch $emergencyPhoneNumber')),
+                        );
+                      }
+                    },
+                    child: Text(emergencyPhoneNumber),
+                  ),
                 ],
               ),
             ),
